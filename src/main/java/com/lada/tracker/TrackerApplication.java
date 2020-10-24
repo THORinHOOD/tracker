09bd45ada478da886c82c4d958a6424bc668ddb8
Asgar Zagitov <ihateyouthor@gmail.com>
2020-10-25 00:24:07 +0300

    fix pipe and added pid write

--- src/main/java/com/lada/tracker/TrackerApplication.java
+++ src/main/java/com/lada/tracker/TrackerApplication.java
@@ -2,12 +2,15 @@ package com.lada.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class TrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrackerApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(TrackerApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter());
+		springApplication.run(args);
 	}
 
 }
